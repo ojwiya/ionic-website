e8d87955982b2b830137e5f741fea0b9d8e33e99
metherton <martinetherton63@gmail.com>
2016-02-15 22:23:19 +0100

    added camera plugin

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,8 @@
 angular.module('conFusion.controllers', [])
 
-  .controller('AppCtrl', function ($scope, $ionicModal, $timeout, $localStorage) {
+  .controller('AppCtrl', function ($scope, $ionicModal, $timeout, $localStorage, $ionicPlatform, $cordovaCamera) {
+
+    $scope.registration = {};
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -68,6 +70,58 @@ angular.module('conFusion.controllers', [])
       $scope.closeReserve();
     }, 1000);
   };
+
+    // Create the registration modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/register.html', {
+      scope: $scope
+    }).then(function (modal) {
+      $scope.registerform = modal;
+    });
+
+    // Triggered in the registration modal to close it
+    $scope.closeRegister = function () {
+      $scope.registerform.hide();
+    };
+
+    // Open the registration modal
+    $scope.register = function () {
+      $scope.registerform.show();
+    };
+
+    // Perform the registration action when the user submits the registration form
+    $scope.doRegister = function () {
+      console.log('Doing reservation', $scope.reservation);
+
+      // Simulate a registration delay. Remove this and replace with your registration
+      // code if using a registration system
+      $timeout(function () {
+        $scope.closeRegister();
+      }, 1000);
+    };
+
+    $ionicPlatform.ready(function() {
+      var options = {
+        quality: 50,
+        destinationType: Camera.DestinationType.DATA_URL,
+        sourceType: Camera.PictureSourceType.CAMERA,
+        allowEdit: true,
+        encodingType: Camera.EncodingType.JPEG,
+        targetWidth: 100,
+        targetHeight: 100,
+        popoverOptions: CameraPopoverOptions,
+        saveToPhotoAlbum: false
+      };
+      $scope.takePicture = function() {
+        $cordovaCamera.getPicture(options).then(function(imageData) {
+          $scope.registration.imgSrc = "data:image/jpeg;base64," + imageData;
+        }, function(err) {
+          console.log(err);
+        });
+
+        $scope.registerform.show();
+
+      };
+    });
 
 })
 
